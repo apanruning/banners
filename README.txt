16dbab23c0c6761fdc76f8813ecb520abb698b24
etnalubma <francisco.herrero@gmail.com>
2009-06-27 12:25:31 -0300

    added the app

---
+++ README.txt
@@ -0,0 +1,4 @@
+A simple accounts app made for use in the "nomenclador cartográfico" of Comercio y Justicia
+
+Quite a broad scope, ah?
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='accounts',
+      version=version,
+      description="a simple user auth app",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ale Peralta, Mat\xc3\xadas Iturburu',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
