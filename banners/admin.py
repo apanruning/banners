8929ccbdeedb9ccc20e403d8d8cc4f54950e727d
etnalubma <francisco.herrero@gmail.com>
2009-06-30 00:07:08 -0300

    First commit that counts.
    This package provides a basic 'banner' model, and a template tag for populating 'slots' in any template

--- banners/admin.py
+++ banners/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from models import Banner
 # Add your admin site registrations here, eg.
 # from banners.models import Author
-# admin.site.register(Author)
+admin.site.register(Banner)
--- banners/models.py
+++ banners/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Banner(models.Model):
+    image = models.ImageField(upload_to='banners')
+    url = models.URLField()
+    slot = models.CharField(max_length=50)
+    clicks = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    
+    
+    
