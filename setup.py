43081577f2f94c42b14163d59855dee894f5cedf
tutuca <maturburu@gmail.com>
2011-04-14 22:27:00 -0300

    Guardando estas pequeñas cosas para más luego

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import os
 
@@ -6,15 +7,13 @@ version = '0.1'
 setup(name='banners',
       version=version,
       description="A simple banner manager app",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='Banners, Django',
-      author='Revoluciones Inform\xc3\xa1ticas',
+      author='Revoluciones Informáticas',
       author_email='info@revolucionesweb.com.ar',
       url='http://revolucionesweb.com.ar',
       license='',
@@ -24,7 +23,7 @@ setup(name='banners',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
+          'django'
       ],
       entry_points="""
       # -*- Entry points: -*-
